0342bc4bd5d987bcdeab88f17897c7b35c1da0fb
its-steff <steffdziadkowiec@proton.me>
2026-03-04 17:50:21 +0100

    experimenting with the CMS

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,17 @@
 const eleventyPluginHandlebars = require("@11ty/eleventy-plugin-handlebars");
+const yaml = require("js-yaml");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyPluginHandlebars);
+  eleventyConfig.addDataExtension("yml", (contents) => yaml.load(contents));
+  eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
   eleventyConfig.addPassthroughCopy("src/admin/config.yml");
   eleventyConfig.addPassthroughCopy("src/assets/uploads");
+  eleventyConfig.addCollection("projects", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("src/content/projects/*.md")
+      .sort((a, b) => b.date - a.date);
+  });
 
   return {
     templateFormats: ["hbs", "html", "md"],
